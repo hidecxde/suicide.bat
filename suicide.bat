deb8d0beabd8f0566c184e3b8443ff557ba07e26
hidecode <141551809+hidec0de@users.noreply.github.com>
2023-08-06 14:41:08 -0300

    Add files via upload

---
+++ suicide.bat
@@ -0,0 +1,48 @@
+@echo off
+color 04
+mode 1000
+title suicide.exe
+cls
+echo Your system has been invaded...
+timeout /t 3 /nobreak >nul
+cls
+echo .
+timeout /t 1 /nobreak >nul
+cls
+echo ..
+timeout /t 1 /nobreak >nul
+cls
+echo ...
+timeout /t 1 /nobreak >nul
+cls
+echo.
+echo                   XXX       XXXX                 
+echo               XXXXXXX       XXXXXXX              
+echo             XXXXXXXXXX      XXXXXXXX            
+echo            XXXXXXXXXXX      XXXXXXXXXX           
+echo           XXXXXXXXXXX       XXXXXXXXXXX          
+echo           XXXXXXXXXXXX      XXXXXXXXXXX          
+echo          X XXXXXXXXXXX   XX XXXXXXXXXXXX         
+echo          X  XXXXXXX      XXXXXXXXXXXXXXX         
+echo          XX XXXXXXXX      XXXXXXXXXXXXXX         
+echo           X  XXXXXXXX  XXXXXXXXXXXXXXXXX         
+echo           X   XXXXXXXX XXXXXXXXXXXXXXXXX         
+echo           X      XXXXXX XXXXXXXXXXXXX X          
+echo           XX      XXXXXXXXXXXXXXXXXXX X          
+echo           X        XXXXXXXXXXXXXXXXXX X          
+echo          X     .:. .XXXXXXXXXXXXXXXXX.:.         
+echo          XXX  .  ..^XXXXXXXXX^..  . XXX          
+echo           XX.         XXXX         XXXX          
+echo            XXXXX       XX ........XXXX           
+echo             XXXXXXXXXXXXXXXXXXXXXXXX             
+echo                  X  XX   :XX  :.                 
+echo                  .. ^. :. :.X..                  
+echo                   :..  :^^....:                  
+echo                    XXXXXXXXXXX                   
+echo                      ::...::
+echo.
+timeout /t 2 /nobreak >nul
+echo Your system will commit suicide! 
+echo You have 30 seconds to save your files until your system dies!
+shutdown -s -t 30
+TIMEOUT 30
